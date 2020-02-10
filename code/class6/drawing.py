a2e995ec9c76d3052c86cca228ea123c7cf1b2a5
Jeremy Pedersen <jeremydavispedersen@gmail.com>
2020-02-10 12:33:48 +0800

    Slight update to PPT and drawing.py code. Added homework and extra credit for Class 6

--- code/class6/drawing.py
+++ code/class6/drawing.py
@@ -21,7 +21,7 @@ pygame.draw.line(DISPLAYSURF, BLUE, (60, 60), (120, 60), 4)
 pygame.draw.line(DISPLAYSURF, BLUE, (120, 60), (60, 120))
 pygame.draw.line(DISPLAYSURF, BLUE, (60, 120), (120, 120), 4)
 pygame.draw.circle(DISPLAYSURF, BLUE, (300, 50), 20, 0)
-pygame.draw.ellipse(DISPLAYSURF, RED, (300, 200, 40, 80), 1)
+pygame.draw.ellipse(DISPLAYSURF, RED, (300, 200, 80, 40), 0)
 pygame.draw.rect(DISPLAYSURF, RED, (200, 150, 100, 50))
 
 pixObj = pygame.PixelArray(DISPLAYSURF)
